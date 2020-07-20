414aec64e27ef2cccfeb76ade4e77d0c7d1e4ad2
monnpk2002 <68544320+monnpk2002@users.noreply.github.com>
2020-07-20 19:02:49 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"1st Term of 2020        \\45132114    Wanthanee Prachuabsupakij\n\n";
+	cout<<"| Subject ID | Subject name             | Score(100) | Grad  |\n\n";
+	cout<<"|  4570034   | Introducting to computer |     85     |   A   |  \n";	
+	cout<<"|  4580247   | English                  |     92     |   B   |  \n";
+	cout<<"|  4570034   | Desjest Thinking         |     85     |   A   |  \n\n";	
+	cout<<"|   TOTAL    | Subject = 3              |  Average = 3.87  |";	
+
+
+
+
+}
+
+
+
+
+
