49a2bc0dc30602f14c71ef1ee4e639e0dd2dce05
monnpk2002 <68544320+monnpk2002@users.noreply.github.com>
2020-07-21 09:10:32 +0700

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -3,12 +3,16 @@ using namespace std;
 int main()
 {
     cout<<"1st Term of 2020        \\45132114    Wanthanee Prachuabsupakij\n\n";
+	cout<<"----------------------------------------------------------------\n";
 	cout<<"| Subject ID | Subject name             | Score(100) | Grad  |\n\n";
+	cout<<"----------------------------------------------------------------\n";
 	cout<<"|  4570034   | Introducting to computer |     85     |   A   |  \n";	
 	cout<<"|  4580247   | English                  |     92     |   B   |  \n";
-	cout<<"|  4570034   | Desjest Thinking         |     85     |   A   |  \n\n";	
-	cout<<"|   TOTAL    | Subject = 3              |  Average = 3.87  |";	
-
+	cout<<"|  4570034   | Desjest Thinking         |     85     |   A   |  \n\n";
+	cout<<"----------------------------------------------------------------\n";
+	cout<<"|   TOTAL    | Subject = 3              |  Average = 3.87  |\n";	
+	cout<<"============================================\n\n\n";
+	cout<<"KMUTNB\n";
 
 
 
